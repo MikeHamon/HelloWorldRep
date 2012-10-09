f26af6ab7d81f5b40d3124f2f3154d2b715b0700
Mike <Mike@Mike-Laptop>
2012-10-09 08:36:38 -0400

    I did this stuff

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public String hello(){
+		return "Hello";
+	}
+
+}
